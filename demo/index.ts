82c0bc98eddd7e42e43e3415bc411be0d0c3ad18
Alexander Myshov <simplex.light@gmail.com>
2023-04-13 20:36:07 +0700

    Extract center into variable

--- demo/index.ts
+++ demo/index.ts
@@ -3,16 +3,17 @@ import { ThreeJsPlugin } from '../src/index';
 
 async function start() {
     const mapglAPI = await load();
+    const mapCenter = [82.886554, 54.980988];
 
     const map = new mapglAPI.Map('container', {
         center: [55.31878, 25.23584],
-        zoom: 18,
+        styleZoom: 18,
         key: 'cb20c5bf-34d3-4f0e-9b2b-33e9b8edb57f',
         pitch: 45,
     });
 
     new ThreeJsPlugin(map, {
-        posLngLat: [55.31878, 25.23584],
+        posLngLat: mapCenter,
     });
 }
 
