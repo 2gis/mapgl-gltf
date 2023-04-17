39248dc4523729f757fcc7791c2abca82837a57a
Alexander Myshov <simplex.light@gmail.com>
2023-04-17 17:46:01 +0700

    Add webpack config for demo

--- webpack.config.js
+++ webpack.config.js
@@ -3,99 +3,80 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = function (env) {
-    const mode = env.production ? 'production' : 'development';
+module.exports = function (env, _argv) {
+    const outputConfig = {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        library: {
+            name: 'mapglThreeJsPlugin',
+            type: 'umd',
+        },
+    };
 
-    if (mode === 'production') {
+    const resolveConfig = {
+        extensions: ['.ts', '.js'],
+        // Fix warning
+        // Multiple instances of Three.js being imported
+        alias: {
+            three: path.resolve('./node_modules/three')
+        },
+    };
+
+    const moduleConfig = {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+            },
+        ],
+    };
+
+    const productionPluginsConfig = [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: 'static',
+                to: 'static',
+            }],
+        })
+    ];
+
+    const developmentPluginsConfig = [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './demo/index.html',
+            filename: 'index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: 'static',
+                to: 'static',
+            }, {
+                from: 'demo/models',
+                to: 'models'
+            }],
+        })
+    ];
+
+    if (env.type === 'production') {
         return {
-            mode,
+            mode: 'production',
             entry: './src/index.ts',
-            output: {
-                path: path.resolve(__dirname, 'dist'),
-                filename: 'bundle.js',
-                library: {
-                    name: 'mapglThreeJsPlugin',
-                    type: 'umd',
-                },
-            },
-            resolve: {
-                extensions: ['.ts', '.js'],
-                // Fix warning
-                // Multiple instances of Three.js being imported
-                alias: {
-                    three: path.resolve('./node_modules/three')
-                },
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.tsx?$/,
-                        loader: 'ts-loader',
-                    },
-                ],
-            },
-            plugins: [
-                new CleanWebpackPlugin(),
-                new HtmlWebpackPlugin({
-                    template: './demo/index.html',
-                    filename: 'index.html',
-                }),
-                new CopyWebpackPlugin({
-                    patterns: [{
-                        from: 'static',
-                        to: 'static',
-                    }, {
-                        from: 'demo/models',
-                        to: 'models'
-                    }],
-                })
-            ],
+            output: outputConfig,
+            resolve: resolveConfig,
+            module: moduleConfig,
+            plugins: productionPluginsConfig,
         };
     }
 
-    if (mode === 'development') {
+    if (env.type === 'development') {
         return {
-            mode,
+            mode: 'development',
             entry: './demo/index.ts',
-            output: {
-                path: path.resolve(__dirname, 'dist'),
-                filename: 'bundle.js',
-                library: {
-                    type: 'umd',
-                },
-            },
-            resolve: {
-                extensions: ['.ts', '.js'],
-                // Fix warning
-                // Multiple instances of Three.js being imported
-                alias: {
-                    three: path.resolve('./node_modules/three')
-                },
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.tsx?$/,
-                        loader: 'ts-loader',
-                    },
-                ],
-            },
-            plugins: [
-                new CleanWebpackPlugin(),
-                new HtmlWebpackPlugin({
-                    template: './demo/index.html',
-                    filename: 'index.html',
-                }),
-                new CopyWebpackPlugin({
-                    patterns: [{
-                        from: 'static',
-                        to: 'static',
-                    }, {
-                        from: 'demo/models',
-                        to: 'models'
-                    }],
-                })
-            ],
+            output: outputConfig,
+            resolve: resolveConfig,
+            module: moduleConfig,
+            plugins: developmentPluginsConfig,
             devServer: {
                 compress: true,
                 port: 3700,
@@ -106,8 +87,17 @@ module.exports = function (env) {
                     writeToDisk: true,
                 },
             },
-            watch: true,
         };
+    }
 
+    if (env.type === 'demo') {
+        return {
+            mode: 'production',
+            entry: './demo/index.ts',
+            output: outputConfig,
+            resolve: resolveConfig,
+            module: moduleConfig,
+            plugins: developmentPluginsConfig,
+        };
     }
 }
