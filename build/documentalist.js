4f85bc6ddef2aad92fa9ffad22a9933c9ef27442
Alexander Myshov <simplex.light@gmail.com>
2023-04-25 22:23:28 +0700

    Add reference generation

---
+++ build/documentalist.js
@@ -0,0 +1,16 @@
+const { Documentalist, TypescriptPlugin } = require('@documentalist/compiler');
+const { mkdirSync, writeFileSync } = require('fs');
+
+mkdirSync('dist', {
+    recursive: true,
+});
+
+new Documentalist()
+    .use(/\.ts$/, new TypescriptPlugin({excludePaths: ['src/utils.ts']}))
+    .documentGlobs('src/**/*')
+    .then((docs) => JSON.stringify(docs))
+    .then((json) => writeFileSync('dist/docs.json', json))
+    .catch((e) => {
+        console.log(e);
+        process.exit(1);
+    });
