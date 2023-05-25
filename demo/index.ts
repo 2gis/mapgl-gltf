3c1a967b77e5fa39b05a4066d785532acb0e24e6
Alexander Myshov <simplex.light@gmail.com>
2023-05-26 05:12:55 +0700

    Generate positions for models in demo

--- demo/index.ts
+++ demo/index.ts
@@ -6,7 +6,7 @@ async function start() {
 
     const map = new mapglAPI.Map('container', {
         center: [82.886554, 54.980988],
-        zoom: 18,
+        zoom: 15.5,
         key: 'cb20c5bf-34d3-4f0e-9b2b-33e9b8edb57f',
         pitch: 45,
         rotation: 330,
@@ -47,27 +47,45 @@ async function start() {
         console.log(e);
     });
 
+    /*
+    const models = [
+        {
+            id: '03a234cb',
+            coordinates: [82.886554, 54.980988],
+            modelUrl: 'models/cube_draco.glb',
+            rotateX: 90,
+            scale: 1000,
+            linkedIds: ['141373143530065', '70030076379181421'],
+        },
+        {
+            id: 'e3a837ff',
+            coordinates: [82.886454, 54.980388],
+            modelUrl: 'models/cube_draco.glb',
+            rotateX: 90,
+            rotateY: 31,
+            scale: 700,
+            linkedIds: ['141373143530064', '70030076379180575'],
+            offsetX: 30,
+        },
+    ];
+    */
+
+    const models = [];
+    for (let i = 0; i < 10; i++) {
+        let lonRnd = (Math.random() / 100) * (Math.random() > 0.5 ? 1 : -1);
+        let latRnd = (Math.random() / 100) * (Math.random() > 0.5 ? 1 : -1);
+        models.push({
+            id: i,
+            coordinates: [82.8865 + lonRnd, 54.9809 + latRnd],
+            modelUrl: 'models/cube_draco.glb',
+            rotateX: 90,
+            scale: 3000,
+            linkedIds: ['141373143530065', '70030076379181421'],
+        });
+    }
+
     plugin
-        .addModels([
-            {
-                id: '03a234cb',
-                coordinates: [82.886554, 54.980988],
-                modelUrl: 'models/cube_draco.glb',
-                rotateX: 90,
-                scale: 1000,
-                linkedIds: ['141373143530065', '70030076379181421'],
-            },
-            {
-                id: 'e3a837ff',
-                coordinates: [82.886454, 54.980388],
-                modelUrl: 'models/cube_draco.glb',
-                rotateX: 90,
-                rotateY: 31,
-                scale: 700,
-                linkedIds: ['141373143530064', '70030076379180575'],
-                offsetX: 30,
-            },
-        ])
+        .addModels(models)
         .then(() => {
             console.log('Models are loaded');
         })
@@ -78,14 +96,14 @@ async function start() {
     plugin.addPoiGroup({
         id: 1,
         type: 'primary',
-        minZoom: 17,
+        minZoom: 15,
         data: {
             type: 'FeatureCollection',
             features: [
                 {
                     type: 'Feature',
                     properties: {
-                        elevation: 30,
+                        elevation: 130,
                         type: 'immersive_poi',
                         label: '3к\n78.4 м²',
                         url: 'https://a101.ru/kvartiry/360810/',
