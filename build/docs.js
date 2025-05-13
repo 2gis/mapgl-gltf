a0d586eb93b22a1416d2c7ad6639c99cc108159e
Dmitry Nishcheta <nish.dima@gmail.com>
2025-05-13 17:41:24 +0500

    Migrate to @2gis/ts-docs-generator

--- build/docs.js
+++ build/docs.js
@@ -1,4 +1,4 @@
-const { generateDocs } = require('@2gis/js-docs-generator');
+const { generateDocs } = require('@2gis/ts-docs-generator');
 const { mkdirSync, writeFileSync } = require('fs');
 const { join } = require('path');
 
@@ -18,6 +18,7 @@ const excludePaths = [
 
 generateDocs({
     version,
+    project: 'gltf',
     defaultReference: 'GltfPlugin',
     docsHost: 'https://unpkg.com/@2gis/mapgl-gltf@^1/dist/docs',
     excludePaths,
